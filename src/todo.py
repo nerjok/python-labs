15ce1c444a86066546b784878efa37109d7e7e87
Nerius Jok <nerius.jok@gmail.com>
2025-10-06 22:51:20 +0300

    file manipulation

--- src/todo.py
+++ src/todo.py
@@ -1,13 +1,33 @@
-todos = []
+# todos = []
 while True:
     userAction = input("Type add, show, edit or exit: ")
     userAction = userAction.strip()
     match userAction:
         case "add":
-            todo = input("Enter todo: ")
+            todo = input("Enter todo: ") +"\n"
+
+            # file = open("src/files/todos.txt", "r")
+            # todos = file.readlines()
+            # file.close()
+
+            with open("src/files/todos.txt", "r") as file:
+                todos = file.readlines()
+
             todos.append(todo)
+
+            # file = open("src/files/todos.txt", "w")
+            # file.writelines(todos)
+            # file.close()
+
+            with open("src/files/todos.txt", "w") as file:
+                file.writelines(todos)
         case "show":
+            with open("src/files/todos.txt", "r") as file:
+                todos = file.readlines()
+    
+            newTodos = [itm.strip("\n") for itm in todos]
             for index, item in enumerate(todos):
+                item = item.strip("\n")
                 print(f"{index+1} - {item}")
         case "exit":
             break
@@ -15,14 +35,23 @@ while True:
             number = input("Number of the todo to edit:")
             number = int(number) - 1
 
+            with open("src/files/todos.txt", "r") as file:
+                todos = file.readlines()
+
             existingTodo = todos[number]
             newTodo = input("enter new todo: ")
-            todos[number] = newTodo
-            print("edit", existingTodo, todos[number])
+            todos[number] = newTodo + "\n"
+            # print("edit", existingTodo, todos[number])
+            with open("src/files/todos.txt", "w") as file:
+                file.writelines(todos)
         case "complete":
             number = input("Number of todo to complete")
             number = int(number)
+            with open("src/files/todos.txt", "r") as file:
+                todos = file.readlines()
             todos.pop(number)
+            with open("src/files/todos.txt", "w") as file:
+                file.writelines(todos)
             print("completed")
 
 print("bye")
